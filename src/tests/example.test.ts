bb0ed9de18bd7631a735d6d6c182fe7b7475b643
Xeffen25 <xeffen25@xeffen25.com>
2026-03-30 20:32:02 +0200

    feat: testing on ci

---
+++ src/tests/example.test.ts
@@ -0,0 +1,15 @@
+// import ComponentName from "@/components/ComponentName.astro";
+import { describe, expect, it } from "vitest";
+
+describe("Example test suite", () => {
+	it("Example test case", async () => {
+		/* const container = await experimental_AstroContainer.create();
+		const result = await container.renderToString(Card, {
+			slots: {
+				default: "Card content",
+			},
+		}); */
+
+		expect("").toBe("");
+	});
+});
