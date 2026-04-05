f9846300607e56754beee7a1e6e5ed4ece3395a7
Xeffen25 <xeffen25@xeffen25.com>
2026-04-05 23:20:49 +0200

    fix: removed csp

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,6 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://eminence-astro-starter.xeffen25.com",
-	security: {
-		checkOrigin: true,
-		csp: true,
-	},
 	adapter: cloudflare(),
 	output: "server",
 });
