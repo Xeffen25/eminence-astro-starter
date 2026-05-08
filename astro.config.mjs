a9aeac6d2b355d610b5d2427088e3430586e3dc2
Xeffen25 <xeffen25@xeffen25.com>
2026-05-08 16:38:37 +0200

    add eminence astro suite

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import eminencesuite from "eminence-astro-suite";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://eminence-astro-starter.xeffen25.com",
 	adapter: cloudflare(),
 	output: "server",
+	integrations: [
+		eminencesuite({
+			icons: {
+				source: "src/assets/icon.svg",
+			},
+		}),
+	],
 });
